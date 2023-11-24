e544cc67fbc11b2fcde398f636a081718d5a4553
tranduykhanhh <127747952+tranduykhanhh@users.noreply.github.com>
2023-11-24 23:16:50 +0700

    get

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,12 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0.z0cmisz.mongodb.net',
+    DB_USER: 'khanh',
+    DB_PASS: 'khanhtran1792003',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: 'tranduykhanh',
+    JWT_EXPIRES: '36000000', // in milliseconds
+    EMAIL_USER: 'khanhtran1792003@gmail.com', // gmail service
+    EMAIL_PASS: 'hwwg nhkc mpsx gwst    '
+  };
+  module.exports = MyConstants;
+  
